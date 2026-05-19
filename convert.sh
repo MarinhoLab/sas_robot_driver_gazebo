4fe9b0a6aa6950289a8ea298e3dd44bc3ce7ecfa
Murilo Marinho <murilo.marinho@manchester.ac.uk>
2026-05-19 10:48:03 +0100

    Generalizing ur conversion script.

--- convert.sh
+++ convert.sh
@@ -1,7 +1,12 @@
 set -e
+
+robot_name=$1
+echo "Converting: $robot_name"
+
 docker build -t srdg_convert --build-context repo=. docker/converter --no-cache
 
+docker container rm extract_files || true
 docker run -t -d --name extract_files srdg_convert /bin/bash
-docker exec extract_files bash -c "cd /root/sas; . convert_ur.sh"
-docker cp extract_files:/root/sas/ur3e.sdf ur3e.sdf
+docker exec extract_files bash -c "cd /root/sas; . convert_ur.sh $robot_name"
+docker cp extract_files:/root/sas/"$robot_name".sdf "$robot_name".sdf
 docker container stop extract_files
--- convert_ur.sh
+++ convert_ur.sh
@@ -1,11 +1,14 @@
 set -e
 
+robot_name=$1
+echo "Converting: $robot_name"
+
 WORKDIR=$(pwd)
 
 source vendor/install/setup.bash
 cd vendor/Universal_Robots_ROS2_Description/urdf
-xacro ur.urdf.xacro name:=test ur_type:=ur3e > ur3e.urdf
-gz sdf -p ur3e.urdf > ur3e.sdf
+xacro ur.urdf.xacro name:="$robot_name"_1 ur_type:="$robot_name" > "$robot_name".urdf
+gz sdf -p "$robot_name".urdf > "$robot_name".sdf
 
-cp ur3e.sdf "$WORKDIR"
+cp "$robot_name".sdf "$WORKDIR"
 cd "$WORKDIR"
