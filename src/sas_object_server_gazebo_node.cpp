a5b055cabf6f56baf5026038530f1e8de3af9ffa
Murilo Marinho <murilo.marinho@manchester.ac.uk>
2026-05-22 14:44:47 +0100

    Fixing headers.

--- src/sas_object_server_gazebo_node.cpp
+++ src/sas_object_server_gazebo_node.cpp
@@ -1,20 +1,20 @@
 /*
 # Copyright (c) 2026 Murilo Marques Marinho
 #
-#    This file is part of sas_common.
+#    This file is part of sas_robot_driver_gazebo.
 #
-#    sas_common is free software: you can redistribute it and/or modify
+#    sas_robot_driver_gazebo is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Lesser General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
 #
-#    sas_common is distributed in the hope that it will be useful,
+#    sas_robot_driver_gazebo is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #    GNU Lesser General Public License for more details.
 #
 #    You should have received a copy of the GNU Lesser General Public License
-#    along with sas_common.  If not, see <https://www.gnu.org/licenses/>.
+#    along with sas_robot_driver_gazebo.  If not, see <https://www.gnu.org/licenses/>.
 #
 # ################################################################
 #
