6cec86ecb8c124bc7909f2c29aedbe5e683bccd8
Murilo Marinho <murilo.marinho@manchester.ac.uk>
2026-05-22 19:53:58 +0100

    Adding subscription example.

--- src/gazebo_subscription_frequency_checker.cpp
+++ src/gazebo_subscription_frequency_checker.cpp
@@ -3,16 +3,20 @@
 #include <string>
 #include <gz/msgs.hh>
 #include <gz/transport.hh>
+#include <sas_core/sas_clock.hpp>
 
-void cb(const gz::msgs::StringMsg &_msg)
+static int message_count = 0;
+
+void cb(const gz::msgs::Pose_V &_msg)
 {
-  std::cout << "Msg: " << _msg.data() << std::endl << std::endl;
+  message_count++;
 }
 
-int main(int argc, char **argv)
+int main(int, char **)
 {
+  sas::Clock clock{0.001};
   gz::transport::Node node;
-  std::string topic = "/foo";
+  std::string topic = "/world/ur3e_world/dynamic_pose/info";
 
   // Subscribe to a topic by registering a callback.
   if (!node.Subscribe(topic, cb))
@@ -21,8 +25,22 @@ int main(int argc, char **argv)
     return -1;
   }
 
+  clock.init();
+  while(message_count < 50)
+  {
+      clock.update_and_sleep();
+  }
+  // Message count
+  std::cout << "Received " << message_count << " messages" << std::endl;
+
+  //Statistics
+  std::cout << "Statistics for the entire loop" << std::endl;
+  std::cout << "  Mean computation time: " << clock.get_statistics(sas::Statistics::Mean,sas::Clock::TimeType::Computational) << std::endl;
+  std::cout << "  Mean idle time: " << clock.get_statistics(sas::Statistics::Mean,sas::Clock::TimeType::Idle) << std::endl;
+  std::cout << "  Mean effective thread sampling time: " << clock.get_statistics(sas::Statistics::Mean,sas::Clock::TimeType::EffectiveSampling) << std::endl;
+
   // Zzzzzz.
-  gz::transport::waitForShutdown();
+  //gz::transport::waitForShutdown();
 
   return 0;
 }
