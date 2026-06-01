f6571f1e4eb1c0c2830cc2afc4a302eafae2dbf6
Murilo Marinho <murilo.marinho@manchester.ac.uk>
2026-06-01 16:46:13 +0100

    [CMakeLists.txt] Making sure the sdf folder is installed.

--- docker/test.sh
+++ docker/test.sh
@@ -3,7 +3,7 @@
 sudo apt-get install tree
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get remove -y ros-jazzy-sas-robot-driver-gazebo
-sudo apt-get install -y xvfb
+sudo apt-get install -y xvfb tree
 sudo /usr/bin/Xvfb :99 -screen 0 1280x1024x24 &
 
 cd /root/sas_robot_driver_gazebo_devel
@@ -11,8 +11,11 @@ ls .
 colcon build
 source install/setup.bash
 
+cd $(ros2 pkg prefix sas_robot_driver_gazebo --share)
+tree .
+
 ros2 run sas_robot_driver_gazebo gazebo_service_frequency_checker
 timeout --signal SIGINT 20 ros2 launch sas_robot_driver_gazebo server_launch.py &
 timeout --signal SIGINT 20 ros2 launch sas_robot_driver_gazebo robot_driver_server_launch.py &
-cd src/sas_robot_driver_gazebo/sdf
-timeout --signal SIGINT 20 gz sim -s ./ur3e_world.sdf
+cd sdf
+timeout --signal SIGINT 20 gz sim -s ./r820_world.sdf
