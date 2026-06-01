be7a7ccb1321f3b1951b814b64d9cfa11e9aed26
Murilo Marinho <murilo.marinho@manchester.ac.uk>
2026-06-01 16:26:00 +0100

    [docker] Making Dockerfile usable externally and test script open a gazebo scene.

--- docker/test.sh
+++ docker/test.sh
@@ -2,15 +2,17 @@
 
 sudo apt-get install tree
 sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get remove -y ros-jazzy-sas-robot-driver-gazebo
+sudo apt-get install -y xvfb
+sudo /usr/bin/Xvfb :99 -screen 0 1280x1024x24 &
 
 cd /root/sas_robot_driver_gazebo_devel
 ls .
 colcon build
 source install/setup.bash
 
-#ros2 run sas_robot_driver_gazebo sas_robot_driver_ros_gazebo.py
 ros2 run sas_robot_driver_gazebo gazebo_service_frequency_checker
-#ros2 run sas_robot_driver_gazebo sas_object_server_gazebo_node
-#ros2 run sas_robot_driver_gazebo sas_simulator_server_gazebo_node
-timeout --signal SIGINT 20 ros2 launch sas_robot_driver_gazebo server_launch.py
-timeout --signal SIGINT 20 ros2 launch sas_robot_driver_gazebo robot_driver_server_launch.py
+timeout --signal SIGINT 20 ros2 launch sas_robot_driver_gazebo server_launch.py &
+timeout --signal SIGINT 20 ros2 launch sas_robot_driver_gazebo robot_driver_server_launch.py &
+cd src/sas_robot_driver_gazebo/sdf
+timeout --signal SIGINT 20 gz sim -s ./ur3e_world.sdf
